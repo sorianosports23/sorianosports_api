892f2420fa04c667e880a8290814c1e502295cf0
efrask7 <efrainschettini.123@gmail.com>
2024-05-31 00:07:46 -0300

    register fix

--- utils/users/addUser.php
+++ utils/users/addUser.php
@@ -1,32 +1,37 @@
 <?php
- 
-  include_once "../database/connection.php";
-  include_once "../utils/errorcodes.php";
 
-  function addUser($username, $fullname, $password, $email, $age, $ci, $phone){
-    global $db;
+include_once "../database/connection.php";
+include_once "../utils/errorcodes.php";
 
-    $response = [
-      "message" => "",
-      "status" => false
-    ];
+function addUser($username, $fullname, $password, $email, $age, $ci, $phone)
+{
+  global $db;
+
+  $response = [
+    "message" => "",
+    "status" => false
+  ];
 
 
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-    $stmt = $db->prepare("INSERT INTO users(username, fullname, password, email, age, ci, phone) VALUES(?,?,?,?,?,?,?)");
-    $stmt->bind_param('sssssii', $username, $fullname, $hashedPassword, $email, $age, $ci, $phone);
+  $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+  $stmt = $db->prepare("
+      INSERT INTO users(username, fullname, password, email, age, ci, phone) 
+      VALUES (:username,:fullname,:password,:email,:age,:ci,:phone)
+    ");
+  $stmt->bindParam('username', $username);
+  $stmt->bindParam('fullname', $fullname);
+  $stmt->bindParam('password', $hashedPassword);
+  $stmt->bindParam('email', $email);
+  $stmt->bindParam('age', $age);
+  $stmt->bindParam('ci', $ci);
+  $stmt->bindParam('phone', $phone);
 
-    if ($stmt->execute()) {
-      $response["message"] = "Usuario añadido";
-      $response["status"] = true;
-      $db->close();
-      return $response;
-    } else {
-      // $errMessage = getMessageError($db->errno);
-      $response["message"] = $stmt->error;
-      $db->close();
-      return $response;
-    }
+  if ($stmt->execute() > 0) {
+    $response["message"] = "Registrado, ya puedes iniciar sesión";
+    $response["status"] = true;
+    return $response;
+  } else {
+    $response["message"] = "No se pudo registra";
+    return $response;
   }
-
-?>
+}
